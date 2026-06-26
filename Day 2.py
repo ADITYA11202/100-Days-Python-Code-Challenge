7490428fc9be0487f507c14df73395de244df739
ADITYA11202 <adityashinde11202@gmail.com>
2026-06-26 21:03:00 +0530

    Day 2

---
+++ Day 2.py
@@ -0,0 +1,25 @@
+# Question 1
+a = int(input("Enter the number: "))
+b = int(input("Enter the number: "))
+
+a,b = b,a
+print("The value of a = ",a)
+print("The value of b = ",b)
+
+print("============================================")
+
+# Question 2
+r = int(input("Enter the radius of circle: "))
+area = 3.14 * r * r
+cir = 6.28 * r
+print(f"The area of circle is : {area}")
+print(f"The circumfrence of circle is : {cir}")
+
+print("============================================")
+
+# Question 3
+c = int(input("Enter the value: "))
+f = 9*c/5 +  32
+print(f"The value in fer is: {f}")
+
+print("============================================")
